f6ba6321429fdfce0ba01b0a92be0edaab1da57f
M-Konvalinka <MikeKonvalinka@gmail.com>
2018-11-28 12:00:04 -0800

    added the ability to submit a new yell but currently it is not correctly passing the information to state, will fix by the end of day

--- src/App.js
+++ src/App.js
@@ -8,22 +8,55 @@ import PersonalInfo from './PersonalInfo/personalinfo';
 
 
 class App extends Component {
-  state = {
-    yells : [
-              { id: '1', content: 'first yell'}, {id: '2', content: 'second yell'}, 
-              { id: '3', content: 'third yell'}, {id: '4', content: 'fourth yell'},
-              { id: '5', content: 'fifth yell'}, {id: '6', content: 'sixth yell'},
-              { id: '7', content: 'seventh yell'}, {id: '8', content: 'eight yell'},
-              { id: '9', content: 'ninth yell'}, {id: '10', content: 'tenth yell'}
-            ],
-    showYells: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      yells : [
+                { id: 1, content: 'first yell'}, { id: 2, content: 'second yell'}, 
+                { id: 3, content: 'third yell'}, { id: 4, content: 'fourth yell'},
+                { id: 5, content: 'fifth yell'}, { id: 6, content: 'sixth yell'},
+                { id: 7, content: 'seventh yell'}, { id: 8, content: 'eight yell'},
+                { id: 9, content: 'ninth yell'}, { id: 10, content: 'tenth yell'}
+              ],
+      showYells: false,
+      defaultYell: 'Type here whats on your mind',
+      newYell: {},
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.addYellHandler = this.addYellHandler.bind(this);
   }
+  
 
   deleteYellHandler = (yellIndex) => {
     const yells = [...this.state.yells];
     yells.splice(yellIndex, 1);
     this.setState({yells: yells})
   }
+
+  
+handleChange = (event) => {
+  this.setState({defaultYell: event.target.value});
+}
+
+  addYellHandler = (event) => {
+    const yells = [...this.state.yells];
+    alert('A new yell was added: ' + this.state.defaultYell);
+    this.setState({newYell : {content: (this.state.defaultYell)}});
+    console.log("the entire new yell being added is " + this.state.newYell);
+    yells.push(this.state.defaultYell);
+    this.setState({yells: yells});
+    event.preventDefault();
+  }
+
+  // handleChange(event) {
+//   this.setState({value: event.target.value});
+// }
+
+// handleSubmit(event) {
+//   alert('A name was submitted: ' + this.state.value);
+//   event.preventDefault();
+// }
 
   toggleYellHandler = () => {
     const doesShow = this.state.showYells;
@@ -49,6 +82,14 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <PersonalInfo className='personalInfo' />
+        <form onSubmit={this.addYellHandler}>
+          <label>
+              What are you thinking?
+                <input type="text" value={this.state.defaultYell} onChange={this.handleChange}/>
+          </label>
+        <input type="submit" value="Submit" />
+        </form>
+        {/* <input type='text'></input><button onClick={this.addYellHandler}>Add Yell</button> */}
         <h5 className={styles.h5Test}> this info is purple and bold due to the 'h5Test' in app.module.css</h5>
         <h2>Below is the dummy Data, which is now green and bold due to 'h5Test' in  yells.module.css</h2>
         <button 
@@ -58,6 +99,36 @@ class App extends Component {
     );
   }
 
-}
+ }  
+//  constructor(props) {
+//   super(props);
+//   this.state = {value: ''};
+
+//   this.handleChange = this.handleChange.bind(this);
+//   this.handleSubmit = this.handleSubmit.bind(this);
+// }
+
+// handleChange(event) {
+//   this.setState({value: event.target.value});
+// }
+
+// handleSubmit(event) {
+//   alert('A name was submitted: ' + this.state.value);
+//   event.preventDefault();
+// }
+
+// render() {
+//   return (
+//     <form onSubmit={this.handleSubmit}>
+//       <label>
+//         Name:
+//         <input type="text" value={this.state.value} onChange={this.handleChange} />
+//       </label>
+//       <input type="submit" value="Submit" />
+//     </form>
+//   );
+// }
+
+
 
 export default App;
--- src/Yells/yells.js
+++ src/Yells/yells.js
@@ -5,7 +5,7 @@ import styles from './yells.module.css'; // need to import css modules styleshee
 const yells = (props) => {
     return (
         // <div className="Yells">
-                <h5 onClick= {props.click} className={`${styles.h5Test} ${styles.yellInformation}`}>{props.content}</h5>
+                <h5 onClick= {props.click} className={`${styles.h5Test} ${styles.yellInformation}`}>The Id of this yell is '{props.id}' , the content is '{props.content}'</h5>
                 // className={`${styles.description} ${styles.yellow}`}
         // </div>
     )
