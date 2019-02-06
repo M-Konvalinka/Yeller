fdf1cf7a2e8716b065f7dcc6fa93398eebfb864a
M-Konvalinka <MikeKonvalinka@gmail.com>
2019-02-05 17:52:28 -0800

    working on logging in and then storing which user is logged in, may start using redux but tbd

--- src/Login/login.js
+++ src/Login/login.js
@@ -7,12 +7,58 @@ class Login extends Component{
             username : '',
             password : '',
         }
+        this.onSubmit = this.onSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        console.log('updated the ' + name);
+        this.setState({
+            [name]: value
+        })
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        console.log(this.state.username.length);
+        console.log(this.state.password.length);
+        // if(this.state.username.length || this.state.password.length < 1){
+        //     console.log('username or password are null');
+        // }
     }
 
     render(){
         return(
             <div>
-                
+                <h1>Please Login</h1>
+                <form onSubmit={this.onSubmit}>
+                  <div>
+                        <div>
+                            <label>UserName:  </label>
+                            <input 
+                                name='username'
+                                type="text" 
+                                value={this.state.username}
+                                onChange={this.handleInputChange} 
+                                />
+                        </div>
+                        <div>
+                            <label>Password:  </label>
+                            <input 
+                                name='password'
+                                type="text" 
+                                value={this.state.password}
+                                onChange={this.handleInputChange} 
+                                />
+                        </div>
+                  </div>
+                    <div>
+                        <input type="submit" value="Register User"/>
+                    </div>
+                </form>               
             </div>
         )
     }
